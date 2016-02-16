dd5191e41d9c389bed18bc5fe43f95dca047b389
wsyy <wsy@dicovi.com>
2016-02-16 12:59:55 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/main/java/com/sprint/sprintweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.sprint.sprintweather.model;
+
+/**
+ * Created by wsy on 2016/2/16.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
